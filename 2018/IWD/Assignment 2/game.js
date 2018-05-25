077f93ea8d4f9355514488f7e5d277d33b5b68f2
Shivneel Achari <hdiproductionsoffical@gmail.com>
2018-05-26 00:32:55 +1200

    Created a ball, added to array and can move. Need to make it better tho.

--- 2018/IWD/Assignment 2/game.js
+++ 2018/IWD/Assignment 2/game.js
@@ -17,29 +17,29 @@ var player = {
 }
 srcX = 0, srcY = 0;
 //------------------------------------------------------------//
-document.body.addEventListener("keydown", function (event) {
+document.body.addEventListener("keydown", function(event) {
   keys[event.keyCode] = true;
 });
 
-document.body.addEventListener("keyup", function (event) {
+document.body.addEventListener("keyup", function(event) {
   keys[event.keyCode] = false;
 });
 
 var background_Image = new Image();
 background_Image.src = "src/assets/sky.jpg";
 background_Image.onload = function() {
- drawBase();
+  drawBase();
 }
 
 var grass_Image = new Image();
 grass_Image.src = "src/assets/sample_grass.png";
-grass_Image.onload = function () {
+grass_Image.onload = function() {
   drawBase();
 }
 
 var player_Image = new Image();
 player_Image.src = "src/assets/player_bird_ss.png";
-player_Image.onload = function () {
+player_Image.onload = function() {
   drawChar();
 }
 //------------------------------------------------------------//
@@ -56,85 +56,61 @@ function drawChar() {
   }
 }
 //------------------------------------------------------------//
-/**
- * Check session buddy for links
- *
- */
+//Object - Circle
+function Seed(x, y, dy, radius, color) {
+  this.x = x;
+  this.y = y;
+  this.dy = dy;
+  this.radius = radius;
+  this.color = color;
 
-var maxSeeds = 10;
-var seedArray = [];
-// define a gradient for circles
-const gradientRadius = 10;
-const grad = ctx.createRadialGradient(0,0,0,0,0,gradientRadius); // gradient 10 pixels radius at 0,0
-grad.addColorStop(1,"rgba(255,0,0,0)");
-grad.addColorStop(0,"rgba(0,0,0,1)");
+  this.update = function() {
+    // if (this.radius == 24px){
+    //   this.dy =- dy;
+    // } else {
+    //   this.radius =+ 2;
+    // }
+    //increase speed
+    if (this.y + this.radius + this.dy < canvas.height) {
+			this.dy =+ 1;
+		} else {
+			this.dy =- 2;
+		}
+		this.y += this.dy;
+		this.draw();
+  }
 
-const gravity = 0.9; // gravity acceleration
-
-// draws a circle using grad (above)
-function drawCircle(x,y,radius){
-    var scale = radius / gradientRadius;
-    ctx.fillStyle = grad;
-    ctx.setTransform(scale, 0, 0, scale, x, y);
+  this.draw = function() {
     ctx.beginPath();
-    ctx.arc(0, 0, radius / scale, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "red"; //radial gradient here
     ctx.fill();
+    ctx.closePath();
+  }
 }
 
-// this object handles all circles. Circles are stored in the array circles.items
-// the function update and draw both return the circles object so that they
-// can be chained.
-const circles = {
-    items : [], // array of circles
-    add(x,y,radius){
-        var circle;
-        circles.items.push(circle = {
-            x,y,radius,
-            dx : 0,  // delta x and y (movement per frame
-            dy : 0,
-        });
-        return circle;
-    },
-    update(){
-        var i,c;
-        for(i = 0; i < circles.items.length; i++){
-            c = circles.items[i]; // get the circle
-            c.dy += gravity;
-            c.x += c.dx;
-            c.y += c.dy;
-            // simulate bounce.. This is the most basic, search SO for better methods
-            if(c.y + c.radius > ctx.canvas.height){
-                c.y = ctx.canvas.height - c.radius;
-                c.dy = -Math.abs(c.dy);  // window resize may cause ball to be moving up when
-                                         // it hits the bottom so need to ensure the bounce is
-                                         // away from the bottom with Math.abs
-            }
-        }
-        return circles;
-    },
-    draw(){
-        var i,c;
-        for(i = 0; i < circles.items.length; i++){
-            c = circles.items[i]; // get the circle
-            drawCircle(c.x,c.y,c.radius);
-        }
-        return circles;
-    }
+//implementation
+var seed;
+var seedArray = [];
+function init() {
+  seed = new Seed(canvas.width/2, canvas.height/2, 2, 30, '30');
+  seedArray.push(seed);
+  console.log(seed);
 }
 
-// main animation loop called once every 1/60th second (if possible)
-// It checks if the widow size matches the canvas and will resize it
-// if not.
-// Then clears the canvas and updates and draws the circles.
-// Then requests the next animation frame
-function mainLoop(time){
-    if(canvas.width !== innerWidth || canvas.height !== innerHeight){ // resize canvas if window size has changed
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-    }
-    ctx.setTransform(1,0,0,1,0,0); // set default transform
-    ctx.clearRect(0,0,canvas.width,canvas.height); // clear the canvas
-    circles.update().draw();
-    requestAnimationFrame(mainLoop);
+function animate() {
+  requestAnimationFrame(animate);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var i = 0; i < seedArray.length; i++){
+    seedArray[i].update();
+  }
 }
-requestAnimationFrame(mainLoop);
+
+//ultilty functions
+function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+init();
+animate();
